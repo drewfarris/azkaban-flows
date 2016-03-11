5ea2e454da9085d56937d90be8920615a2131591
Drew Farris <drew.farris@gmail.com>
2016-03-11 08:41:55 -0500

    Hadoop Jobs via the command job runner, using scripts

---
+++ random-word-stats/generate-words.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+HADOOP_HOME="/usr/lib/hadoop"
+HADOOP_CONF_DIR="/etc/hadoop/conf"
+HADOOP_BIN="/usr/bin/hadoop"
+
+MAPREDUCE_BASE="/usr/lib/hadoop-mapreduce"
+EXAMPLES_JAR="${MAPREDUCE_BASE}/hadoop-mapreduce-examples-2.6.0-cdh5.4.2.jar"
+
+OUTPUT_DIR="random-words"
+
+${HADOOP_BIN} jar ${EXAMPLES_JAR} randomtextwriter ${OUTPUT_DIR} \
+    -Dmapreduce.randomtextwriter.totalbytes=104857600 \
+    -Dmapreduce.randomtextwriter.bytespermap=104857600
+
---
+++ random-word-stats/word-median.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+HADOOP_HOME="/usr/lib/hadoop"
+HADOOP_CONF_DIR="/etc/hadoop/conf"
+HADOOP_BIN="/usr/bin/hadoop"
+
+MAPREDUCE_BASE="/usr/lib/hadoop-mapreduce"
+EXAMPLES_JAR="${MAPREDUCE_BASE}/hadoop-mapreduce-examples-2.6.0-cdh5.4.2.jar"
+
+INPUT_DIR="random-words"
+OUTPUT_DIR="word-median"
+
+${HADOOP_BIN} jar ${EXAMPLES_JAR} wordcount ${INPUT_DIR} ${OUTPUT_DIR} \
+    -Dmapreduce.randomtextwriter.totalbytes=104857600 \
+    -Dmapreduce.randomtextwriter.bytespermap=104857600
